c33927bc179b9fd46fdb07a7f76162370447e847
Julien WALLE <jwalle@e3r8p20.42.fr>
2015-08-10 15:18:34 +0200

    quick push

--- tiny.c
+++ tiny.c
@@ -37,62 +37,65 @@ t_tiny	*tiny_fill(t_tiny *head)
 	return (tiny);
 }
 
-t_block	*tiny_block_init(size_t size, void *ptr, size_t size_total)
+t_block	*block_init(size_t size, void *ptr, size_t size_total, t_block *head)
 {
 	t_block *block;
 
 	block = mmap(0, sizeof(t_block), FLAGS_PROT, FLAGS_MAP , -1, 0);
+	block->next = head;
+	head = block;
 	block->size = size;
 	block->start = ptr + size_total;
-	block->next = NULL;
 	return (block);
 }
 
-t_block	*tiny_block_fill(size_t size, void *ptr, size_t size_total, t_block *head)
+t_block	*block_fill(size_t size, void *ptr, size_t size_total, t_block *cur)
 {
 	t_block *block;
 
 	block = mmap(0, sizeof(t_block), FLAGS_PROT, FLAGS_MAP , -1, 0);
+	cur->next = block;
+	block->next = NULL;
 	block->size = size;
 	block->start = ptr + size_total;
-	block->next = NULL;
-	head->next = block;
 	return (block);
 }
 
 void	*get_tiny(size_t size)
 {
-	//printf("TINY\n");
+	printf("TINY\n");
 	t_tiny	*tiny;
-	//t_block	*block;
+	t_block	*block;
+
+	if (!g_env.tiny)
+		g_env.tiny = tiny_init();
 
 	tiny = g_env.tiny;
-	
-	if (!tiny)
-		tiny = tiny_init();
 
 	while (tiny->next != NULL)
 		tiny = tiny->next;
 
-	//block = tiny->block;
+	block = tiny->block;
 
 	if ((tiny->size + size > TINY_SIZE * 100))
 	{
 		tiny = tiny_fill(tiny);
-		tiny->block = tiny_block_init(size, tiny->start, tiny->size);
-		return(tiny->block->start);	
+		block = block_init(size, tiny->start, tiny->size, block);
+		return(block->start);	
 	}
 
-	while(tiny->block->next != NULL)
-		tiny->block = tiny->block->next;
+	while (block && block->next != NULL)
+		block = block->next;
 
-	if (!tiny->start)
-		tiny->start = mmap(0, TINY_SIZE * 100, FLAGS_PROT, FLAGS_MAP , -1, 0);
+	printf("post next\n");
 
-	tiny->block = tiny_block_fill(size, tiny->start, tiny->size, tiny->block);
+	block = block_fill(size, tiny->start, tiny->size, block);
 	tiny->size += size;
 
-	return(tiny->block->start);
+	printf("PLPOPLOP\n");
+
+
+	return(block->start);
 }
 
 
