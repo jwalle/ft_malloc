44f4c9a980241da8815ab38d399e583dc2e62707
Julien WALLE <jwalle@e2r11p19.42.fr>
2015-09-13 15:38:26 +0200

    test program

--- print_win.sh
+++ print_win.sh
@@ -2,9 +2,17 @@
 #for ((x = 0; x < 20; x++)); do
 #printf %s -
 cols=$(tput cols)
-printf '%*s\n' "${COLUMNS:-$cols}" '' |  tr ' ' -
-for ((x = 0; x < 4; x++)); do
-	printf '%s%*s%s\n' "|" "${COLUMNS:-$((cols - 1))}" "|"
+first=$(((cols / 2) + 7))
+second=$(((cols / 2) - 7))
+printf '%*s\n' "$cols" '' |  tr ' ' -
+for ((x = 0; x < 1; x++)); do
+	printf '%s%*s%s\n' "|" "$((cols - 1))" "|"
 done
-printf '%*s\n' "${COLUMNS:-$cols}" '' |  tr ' ' -
+for ((x = 0; x < 1; x++)); do
+	printf '%s%*s%s%*s%s\n' "|" "$first" " " "MALLOC" "$second" " " "|"
+done
+for ((x = 0; x < 2; x++)); do
+	printf '%s%*s%s\n' "|" "$((cols - 1))" "|"
+done
+printf '%*s' "$cols" '' |  tr ' ' -
 echo
